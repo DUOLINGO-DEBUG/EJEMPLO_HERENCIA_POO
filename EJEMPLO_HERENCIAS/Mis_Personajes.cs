c5c6aa7e3345bb4ba898a9582d086f88a12a224c
DUOLINGO-DEBUG <149110830+DUOLINGO-DEBUG@users.noreply.github.com>
2025-07-26 21:14:12 -0600

    v1.0.0

---
+++ EJEMPLO_HERENCIAS/Mis_Personajes.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace EJEMPLO_HERENCIAS
+{
+    class PERSONAJE
+    {
+        //[+] ATRIBUTOS
+        private string nombre;
+        private int vida;
+        private int inteligencia;
+        private string skins;
+
+        //Recuerde que esto se debe dejar como privado para evitar que
+        //los atributos sean modificados sin ninguna seguridad
+
+        //[+] CONSTRUCTOR
+        public PERSONAJE(string nombre, int vida, int inteligencia, string skins)
+        {
+            this.nombre = nombre;
+            this.vida = vida;
+            this.inteligencia = inteligencia;
+            this.skins = skins;
+        }
+
+        //Recuerden que el constructor es como la instruciones de la receta
+        //allí definimos los datos necesarios para crear el objeto
+
+        public string Obtener_Nombre
+        {
+            get { return nombre; }
+        }
+        public string Modificar_Nombre
+        {
+            set { nombre = value; }
+        }
+        public int Obtener_Vida
+        {
+            get { return vida; }
+        }
+
+        public int Modificar_Vida
+        {
+            set { vida = value; }
+        }
+
+        public int Obtener_inteligencia
+        {
+            get { return inteligencia; }
+        }
+
+        public int Modificar_inteligencia
+        {
+            set { inteligencia = value; }
+        }
+
+        public string Obtener_skins
+        {
+            get { return skins; }
+        }
+
+        public string Modificar_skins
+        {
+            set { skins = value; }
+        }
+
+        public virtual void Atacar()
+        {
+            Console.WriteLine($" {nombre} hace un ataque generico");
+        }
+
+        public virtual void Hacer_sonido()
+        {
+
+        }
+
+        //[virtual] Permite que un método en la clase base pueda ser modificado por las clases mago y guerrero.
+    }
+
+    class MAGO : PERSONAJE
+    {
+        private int magia;
+        private string pociones;
+        public MAGO(string nombre, int vida, int inteligencia, string skins, int magia, string pociones) : base (nombre, vida, inteligencia, skins)
+        {
+            this.magia = magia;
+            this.pociones = pociones;
+        }
+
+        public int Obtener_Magia
+        {
+            get { return magia; }
+        }
+
+        public int Modificar_Magia
+        {
+            set { magia = value; }
+        }
+
+        public string Obtener_Pociones
+        {
+            get { return pociones; }
+        }
+
+        public string Modificar_Pociones
+        {
+            set { pociones = value; }
+        }
+
+        public override void Atacar()
+        {
+            Console.WriteLine($" {Obtener_Nombre} realiza un hechizo");
+        }
+
+        public override void Hacer_sonido()
+        {
+            Console.WriteLine("Miau");
+        }
+
+        //[override] Se usa en las clases hijas para personalizar el comportamiento del método heredado.
+    }
+
+    class GUERRERO : PERSONAJE
+    {
+        private int fuerza;
+        private string arma;
+
+        public GUERRERO(string nombre, int vida, int inteligencia, string skins, int fuerza, string arma) :
+            base (nombre, vida, inteligencia, skins)
+        {
+            this.fuerza = fuerza;
+            this.arma = arma;
+        }
+
+        public int Obtener_Fuerza
+        {
+            get { return fuerza; }
+        }
+
+        public int Modificar_Fuerza
+        {
+            set 
+            {
+                if(value < 100)
+                {
+                    fuerza = value;
+
+                }
+                else
+                {
+                    fuerza = 100;
+                }
+            }
+        }
+
+        public string Obtener_Arma
+        {
+            get { return arma; }
+        }
+
+        public string Modificar_Arma
+        {
+            set { arma = value; }
+        }
+
+        public override void Atacar()
+        {
+            Console.WriteLine($" {Obtener_Nombre} realiza un golpe con {arma}");
+        }
+
+        public void Encendido()
+        {
+
+        }
+
+        public override void Hacer_sonido()
+        {
+            Console.WriteLine("Gaua");
+        }
+    }
+}
---
+++ EJEMPLO_HERENCIAS/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace EJEMPLO_HERENCIAS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MAGO Mago1 = new MAGO("chala", 100, 2, "black_user.png", 50, "Visión Nocturna");
+
+            Console.WriteLine("//ESTADISTICAS DEL PERSONAJE " + Mago1.Obtener_Nombre);
+            Console.WriteLine("Nombre: " + Mago1.Obtener_Nombre);
+            Console.WriteLine("Inteligencia" + Mago1.Obtener_inteligencia);
+            Console.WriteLine("Skins: " + Mago1.Obtener_skins);
+            Console.WriteLine("Magia: " + Mago1.Obtener_Magia);
+            Console.WriteLine("Poción: " + Mago1.Obtener_Pociones);
+            Console.WriteLine("");
+
+            Mago1.Atacar();
+            
+            Console.WriteLine("");
+            Console.WriteLine("////////////////////////////////////////////////////////////////");
+
+            GUERRERO Guerrero1 = new GUERRERO("Casimiro", 100, 2, "white_user.png", 200, "Machete");
+
+            Console.WriteLine("//ESTADISTICAS DEL PERSONAJE " + Guerrero1.Obtener_Nombre);
+            Console.WriteLine("Nombre: " + Guerrero1.Obtener_Nombre);
+            Console.WriteLine("Inteligencia" + Guerrero1.Obtener_inteligencia);
+            Console.WriteLine("Skins: " + Guerrero1.Obtener_skins);
+            Console.WriteLine("Fuerza: " + Guerrero1.Obtener_Fuerza);
+            Console.WriteLine("Arma: " + Guerrero1.Obtener_Arma);
+            Console.WriteLine("");
+
+            Guerrero1.Atacar();
+
+
+        }
+    }
+}
